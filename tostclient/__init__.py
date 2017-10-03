c14363ba570a184e010a73fd9e473aec5588842d
Ezzeri Esa <esa@squareup.com>
2017-10-03 15:24:40 -0700

    enabled bencode decoding on client

--- tostclient/__init__.py
+++ tostclient/__init__.py
@@ -1,7 +1,7 @@
+import re
 import os
 import requests
-
-from helpers import decode_response
+from itertools import izip
 
 
 class TostClient(object):
@@ -130,3 +130,43 @@ class TostClient(object):
                 "parent-access-token": parent_access_token
             }
         }
+
+
+def decode_bencode(string):
+
+    def decode(string):
+        digits = [str(item) for item in xrange(10)]
+
+        if string == "":
+            return None, ""
+
+        elif string.startswith("i"):
+            match = re.match("i(-?\d+)e", string)
+            return int(match.group(1)), string[match.span()[1]:]
+
+        elif any([string.startswith(item) for item in digits]):
+            match = re.match("(\d+):", string)
+            start = match.span()[1]
+            end = start + int(match.group(1))
+            return string[start:end], string[end:]
+
+        elif string.startswith("l") or string.startswith("d"):
+            elements = []
+            rest = string[1:]
+            while not rest.startswith("e"):
+                element, rest = decode(rest)
+                elements.append(element)
+            rest = rest[1:]
+            if string.startswith("l"):
+                return elements, rest
+            else:
+                return {k: v for k, v in izip(elements[::2], elements[1::2])}, rest
+
+        else:
+            raise ValueError("Malformed string.")
+
+    return decode(string)[0]
+
+
+def decode_response(response):
+    return decode_bencode(response.json()["content"])
